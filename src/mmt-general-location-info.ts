241462d354c3ffea9c605d89f17d20f336587fb4
otya <otya281@gmail.com>
2026-01-11 14:00:47 +0900

    MMTIPv6Dataflow追加

--- src/mmt-general-location-info.ts
+++ src/mmt-general-location-info.ts
@@ -1,9 +1,17 @@
 import { BinaryReader } from "./utils";
 
-export type MMTGeneralLocationInfo = MMTSameDataflowLocationInfo;
+export type MMTGeneralLocationInfo = MMTSameDataflowLocationInfo | MMTIPv6Dataflow;
 
 export type MMTSameDataflowLocationInfo = {
     locationType: "sameDataflow";
+    packetId: number;
+};
+
+export type MMTIPv6Dataflow = {
+    locationType: "ipv6Dataflow";
+    ipv6SourceAddress: Uint8Array;
+    ipv6DestAddress: Uint8Array;
+    destPort: number;
     packetId: number;
 };
 
@@ -21,6 +29,17 @@ export function readMMTGeneralLocationInfo(
             }
             return {
                 locationType: "sameDataflow",
+                packetId: reader.readUint16(),
+            };
+        case 0x02:
+            if (!reader.canRead(16 + 16 + 2 + 2)) {
+                return undefined;
+            }
+            return {
+                locationType: "ipv6Dataflow",
+                ipv6SourceAddress: reader.slice(16),
+                ipv6DestAddress: reader.slice(16),
+                destPort: reader.readUint16(),
                 packetId: reader.readUint16(),
             };
         default:
